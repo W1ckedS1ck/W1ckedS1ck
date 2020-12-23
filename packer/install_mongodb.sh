f8688751adbfff0acc0700b7f61afdc4311b72f4
W1ckedS1ck <61629889+W1ckedS1ck@users.noreply.github.com>
2020-12-23 18:22:15 +0300

    Delete install_mongodb.sh

--- packer/install_mongodb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
-sudo apt-get update
-sudo apt-get install mongodb-org -y --allow-unauthenticated
-sudo systemctl start mongod
-sudo systemctl enable mongod
-sudo systemctl status mongod
-
